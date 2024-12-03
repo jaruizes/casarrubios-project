aea8d3d8c93ea524518523ad5c94d2cf214ca673
Jose Alberto Ruiz Casarrubios <jalb80@gmail.com>
2024-12-03 23:49:27 +0100

    - Added schema "candidates" - Refactored services to use the schema

--- platform/postgresql/inventory.sql
+++ platform/postgresql/inventory.sql
@@ -1,3 +1,4 @@
+--- CANDIDATES ---
 CREATE SCHEMA IF NOT EXISTS CANDIDATES;
 
 -- Tabla principal para las posiciones abiertas
@@ -13,7 +14,7 @@ CREATE TABLE CANDIDATES.POSITIONS
 CREATE TABLE CANDIDATES.REQUIREMENTS
 (
     id          SERIAL PRIMARY KEY,
-    position_id INT  NOT NULL REFERENCES CANDIDATES.POSITIONS (id) ON DELETE CASCADE,
+    position_id INT  NOT NULL REFERENCES positions (id) ON DELETE CASCADE,
     description TEXT NOT NULL
 );
 
@@ -21,17 +22,36 @@ CREATE TABLE CANDIDATES.REQUIREMENTS
 CREATE TABLE CANDIDATES.CONDITIONS
 (
     id          SERIAL PRIMARY KEY,
-    position_id INT  NOT NULL REFERENCES CANDIDATES.POSITIONS (id) ON DELETE CASCADE,
+    position_id INT  NOT NULL REFERENCES positions (id) ON DELETE CASCADE,
     description TEXT NOT NULL
 );
 
 
-INSERT INTO CANDIDATES.positions (id, title, description)
-VALUES (1, 'Arquitecto Software', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet nisl urna, vel convallis felis ultrices at. Aliquam velit ante, interdum sit amet eros vel, consectetur hendrerit nunc. Sed vulputate felis risus, eu suscipit urna lobortis quis. Nullam egestas viverra quam a iaculis. Etiam mattis, ante in dapibus tristique, sem felis porta nunc, quis cursus est nibh at leo. Morbi facilisis ullamcorper lacus, at imperdiet arcu fringilla pharetra. Donec nisi lectus, lobortis eget vestibulum sollicitudin, fringilla non odio. Donec at mollis erat. Fusce dui enim, maximus in metus gravida, sodales suscipit lorem. Donec tempor consequat consectetur.
+
+CREATE TABLE CANDIDATES.APPLICATIONS
+(
+    id         VARCHAR(255) PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    surname    VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    phone      VARCHAR(255) NOT NULL,
+    cv         VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+INSERT INTO CANDIDATES.positions (id, title, description, created_at)
+VALUES (1, 'Arquitecto Software', e 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet nisl urna, vel convallis felis ultrices at. Aliquam velit ante, interdum sit amet eros vel, consectetur hendrerit nunc. Sed vulputate felis risus, eu suscipit urna lobortis quis. Nullam egestas viverra quam a iaculis. Etiam mattis, ante in dapibus tristique, sem felis porta nunc, quis cursus est nibh at leo. Morbi facilisis ullamcorper lacus, at imperdiet arcu fringilla pharetra. Donec nisi lectus, lobortis eget vestibulum sollicitudin, fringilla non odio. Donec at mollis erat. Fusce dui enim, maximus in metus gravida, sodales suscipit lorem. Donec tempor consequat consectetur.
+
 Aenean congue, ante efficitur mollis cursus, turpis mauris aliquet justo, et aliquet nisl diam et nisi. Ut quis metus ac velit eleifend gravida sed eu ligula. Aenean lectus lacus, consequat vitae dictum a, ultricies at odio. Nunc tincidunt nisl et nisi sagittis iaculis. Integer blandit risus at lorem placerat fermentum. Integer malesuada molestie turpis sed viverra. Sed suscipit eget odio at commodo. Praesent lacinia ipsum non nunc volutpat, et molestie urna vulputate. Maecenas rhoncus maximus dolor, ut consectetur libero vehicula vitae.
+
 Duis varius in ante posuere sollicitudin. Nullam hendrerit volutpat porttitor. Sed porta feugiat eros, vitae molestie ligula cursus ut. Donec eget odio non enim lacinia vulputate id ut nunc. Nam mattis eros vel lorem porttitor, ut posuere dui bibendum. Sed eget urna elementum, tempor ipsum nec, pharetra velit. Nam lectus orci, pretium a nibh nec, maximus efficitur nulla. Sed at dignissim erat, ac pellentesque mi. Nam aliquam felis ut ultricies interdum. Quisque eu vehicula ex, quis fermentum nisi. Donec non ultricies ex. Nunc vitae dolor nibh. Donec maximus ipsum vel felis tincidunt vulputate. Proin tristique eros eu sapien vulputate lobortis.
+
 Maecenas viverra odio orci, ac aliquam lacus dapibus eget. Suspendisse potenti. Vivamus a tellus quis turpis efficitur blandit. Integer tempor molestie orci ac suscipit. Praesent vitae tortor sollicitudin, ornare ligula at, dictum magna. Ut maximus lorem at eros finibus sodales. Sed sed aliquam enim. Aliquam ut fringilla est. Cras vel ullamcorper urna.
-Donec placerat metus a posuere facilisis. Praesent a massa neque. Vestibulum at nibh pretium, tristique arcu a, varius dui. Pellentesque id tincidunt mauris. Vivamus nec nisi at ex tristique luctus eget sit amet leo. Nam efficitur felis sapien, tempor accumsan nunc auctor quis. Donec semper auctor arcu ut lacinia. Nulla egestas, quam malesuada ultrices ullamcorper, neque turpis aliquam sapien, sit amet tincidunt augue est quis lorem. Duis nec iaculis libero. Nulla ut varius lorem.');
+
+Donec placerat metus a posuere facilisis. Praesent a massa neque. Vestibulum at nibh pretium, tristique arcu a, varius dui. Pellentesque id tincidunt mauris. Vivamus nec nisi at ex tristique luctus eget sit amet leo. Nam efficitur felis sapien, tempor accumsan nunc auctor quis. Donec semper auctor arcu ut lacinia. Nulla egestas, quam malesuada ultrices ullamcorper, neque turpis aliquam sapien, sit amet tincidunt augue est quis lorem. Duis nec iaculis libero. Nulla ut varius lorem.',
+        '2024-11-28 00:55:04.000000');
 
 
 INSERT INTO CANDIDATES.requirements (id, position_id, description)
--- project/candidates/backend/services/positions-service/src/main/java/com/jaruiz/casarrubios/candidates/services/positions/adapters/persistence/postgresql/PostgresRepository.java
+++ project/candidates/backend/services/positions-service/src/main/java/com/jaruiz/casarrubios/candidates/services/positions/adapters/persistence/postgresql/PostgresRepository.java
@@ -2,6 +2,7 @@ package com.jaruiz.casarrubios.candidates.services.positions.adapters.persistenc
 
 import com.jaruiz.casarrubios.candidates.services.positions.adapters.persistence.postgresql.entities.PositionEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostgresRepository extends CrudRepository<PositionEntity, Long> {
