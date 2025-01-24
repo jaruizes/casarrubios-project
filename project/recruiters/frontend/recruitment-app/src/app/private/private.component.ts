842f7932f67377387225f289b15598d9da3b1036
Jose Alberto Ruiz Casarrubios <jalb80@gmail.com>
2025-01-24 15:40:00 +0100

    - Added footer component - Added home button to header

--- project/recruiters/frontend/recruitment-app/src/app/private/private.component.ts
+++ project/recruiters/frontend/recruitment-app/src/app/private/private.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'private-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './private.component.html',
   styleUrl: './private.component.scss'
 })
