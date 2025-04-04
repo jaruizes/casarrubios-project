90fcabfd3ed541d551df42b896436274ada49f93
Jose Alberto Ruiz Casarrubios <jalb80@gmail.com>
2025-04-04 13:15:09 +0200

    Improved telemetry

--- platform/local/cdc/register-connectors.sh
+++ platform/local/cdc/register-connectors.sh
@@ -1,11 +1,11 @@
 echo "Registrando conector <applications-cdc-postgresql-connector>..."
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://cdc-service:8083/connectors/ -d @/connectors/applications-cdc-postgresql-connector.json
-sleep 60
+sleep 20
 
 
 echo "Registrando conector <recruiters-positions-cdc-connector>..."
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://cdc-service:8083/connectors/ -d @/connectors/recruiters-positions-cdc-connector.json
-sleep 60
+sleep 20
 
 
 echo "Registrando conector <recruiters-new-applications-outbox-connector>..."
