e4e1da3f2ccb59733cc932a6ad1f9acc1f7f25c8
Jose Alberto Ruiz Casarrubios <jalb80@gmail.com>
2025-05-07 22:46:02 +0200

    Refactored and fixed error

--- platform/k8s/cdc/register-connectors-k8s.sh
+++ platform/k8s/cdc/register-connectors-k8s.sh
@@ -5,15 +5,10 @@ echo "Using database host: $DATABASE_HOST"
 echo "Using database name: $DATABASE_NAME"
 echo "Using database name: $CDC_SERVICE"
 
-# List of connector files to update
-connector_files=(
-  "/connectors/recruiters-new-applications-outbox-connector.json"
-  "/connectors/recruiters-positions-cdc-connector.json"
-  "/connectors/applications-cdc-postgresql-connector.json"
-)
+connector_files="/connectors/recruiters-new-applications-outbox-connector.json /connectors/recruiters-positions-cdc-connector.json /connectors/applications-cdc-postgresql-connector.json"
 
 # Update the database hostname and name in each connector file
-for file in "${connector_files[@]}"; do
+for file in $connector_files; do
   if [ -f "$file" ]; then
     echo "Updating configuration in $file"
 
@@ -34,7 +29,7 @@ done
 
 # Wait for CDC service to be available
 echo "Waiting for CDC service to be available..."
-until curl -s http://cdc-service:8083/connectors/ > /dev/null; do
+until curl -s http://${CDC_SERVICE}:8083/connectors/ > /dev/null; do
   echo "CDC service is unavailable - sleeping 5s"
   sleep 5
 done
@@ -42,14 +37,15 @@ echo "CDC service is up!"
 
 # Register connectors
 echo "Registrando conector <applications-cdc-postgresql-connector>..."
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://cdc-service:8083/connectors/ -d @/connectors/applications-cdc-postgresql-connector.json
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://${CDC_SERVICE}:8083/connectors/ -d @/connectors/applications-cdc-postgresql-connector.json
 sleep 20
 
 echo "Registrando conector <recruiters-positions-cdc-connector>..."
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://cdc-service:8083/connectors/ -d @/connectors/recruiters-positions-cdc-connector.json
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://${CDC_SERVICE}:8083/connectors/ -d @/connectors/recruiters-positions-cdc-connector.json
 sleep 20
 
 echo "Registrando conector <recruiters-new-applications-outbox-connector>..."
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://cdc-service:8083/connectors/ -d @/connectors/recruiters-new-applications-outbox-connector.json
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://${CDC_SERVICE}:8083/connectors/ -d @/connectors/recruiters-new-applications-outbox-connector.json
 
 echo "All connectors registered successfully!"
+
